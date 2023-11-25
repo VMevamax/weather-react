49c0192ae264cc39d83c0b9379c51e91445e77c5
VMevamax <132147870+VMevamax@users.noreply.github.com>
2023-11-25 22:50:37 +0200

    declear2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Search from "./Search";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <div className="App">
+      <h1>Weather App</h1>
+      <Search />
+    </div>
   </StrictMode>
 );
